d82d1dc7d912a28fd94a64fe06f59b2ef677dc06
Andreas Bayu Prakasa <abayup1498@gmail.com>
2024-02-22 14:47:33 +0700

    V1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.native160421058"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.native160421058"
